26d1234888f6bbaece7ff5c720ff4d65f98188fe
torhen <torhen@gmail.com>
2021-03-15 23:57:07 +0100

    Add files via upload

---
+++ test.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+function setup() {
+    createCanvas(400, 400);
+}
+function draw() {
+    background(0, 255, 0);
+}
---
+++ test.ts
@@ -0,0 +1,9 @@
+import * as p5Global from 'p5/global' 
+
+function setup():void {
+  createCanvas(400, 400);
+}
+
+function draw():void {
+  background(0, 255, 0);
+}
