38d294d9d82c4df164537b455e6959760675088b
torhen <torhen@gmail.com>
2021-10-08 12:39:45 +0200

    Add files via upload

--- MinimalWindow.cpp
+++ MinimalWindow.cpp
@@ -1,6 +1,7 @@
 #include <windows.h>
+// Application can't be properly closed
 
-int _stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+int _stdcall WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int) {
 	HWND hWnd;
 	WNDCLASS wc;
 	MSG msg;
@@ -8,11 +9,11 @@ int _stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 	wc = {};
 	wc.lpfnWndProc = DefWindowProc;
 	wc.hInstance = hInstance;
-	wc.lpszClassName = L"TEST_CLASS";
+	wc.lpszClassName = L"MY_CLASS";
 
 	RegisterClass(&wc);
 
-	hWnd = CreateWindow(L"TEST_CLASS", L"MinimalWindow", WS_OVERLAPPEDWINDOW|WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 800, 600, 0, 0, hInstance, 0);
+	hWnd = CreateWindow(L"MY_CLASS", L"MinimalWindow", WS_OVERLAPPEDWINDOW|WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 800, 600, 0, 0, hInstance, 0);
 
 	while (true) {
 		GetMessage(&msg, 0, 0, 0);
