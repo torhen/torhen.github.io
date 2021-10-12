6912cb4a7d1ba54023d6618594f972ce90306c8b
torhen <torhen@gmail.com>
2021-10-12 14:47:27 +0200

    Add files via upload

--- 05_ShowBitmap.cpp
+++ 05_ShowBitmap.cpp
@@ -1,62 +1,49 @@
 #include <windows.h>
-//http://www.winprog.org/tutorial/bitmaps.html
 
 HBITMAP g_hBitmap;
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam) {
-
-	PAINTSTRUCT ps;
-	HDC hdcMem;
-	HDC hdc;
+	PAINTSTRUCT ps = {};
+	HDC hDC, hdcMem;
+	RECT rect = { 10, 10, 100, 100 };
 	BITMAP bm;
 
 	switch (Msg) {
 	case WM_CREATE:
-		g_hBitmap = (HBITMAP)LoadImage(0, L"C:\\test\\test.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
-
+		g_hBitmap = (HBITMAP)LoadImage(0, L"test.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
 		if (g_hBitmap == 0) {
-			MessageBox(0, L"Could not load BMP", 0, 0);
+			MessageBox(0, L"Error loading bitmap", 0, 0);
 		}
 
 	case WM_PAINT:
 
-		hdc = BeginPaint(hWnd, &ps);
+		hDC = BeginPaint(hWnd, &ps);
 
-		hdcMem = CreateCompatibleDC(hdc);
-
-		// test g_bitmap not zeros to get rid of warnings
-		if (g_hBitmap) {
-			SelectObject(hdcMem, g_hBitmap);
-			// Just to get size of bitmap
-			GetObject((HGDIOBJ)g_hBitmap, sizeof(bm), &bm);
-			BitBlt(hdc, 10, 10, bm.bmWidth, bm.bmHeight, hdcMem, 0, 0, SRCCOPY);
-		}
+		hdcMem = CreateCompatibleDC(hDC);
+		SelectObject(hdcMem, g_hBitmap);
+		BitBlt(hDC, 100, 100, 1000, 10000, hdcMem, 0, 0, SRCCOPY);
 
 		DeleteDC(hdcMem);
 
 		EndPaint(hWnd, &ps);
 		return 0;
-
 	case WM_DESTROY:
 		PostQuitMessage(0);
+		return 0;
 	}
 	return DefWindowProc(hWnd, Msg, wParam, lParam);
 }
 
-int _stdcall WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ char*, _In_ int) {
-
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ char*, _In_ int) {
 	WNDCLASS wc = {};
+	wc.hInstance = hInstance;
 	wc.lpfnWndProc = WndProc;
 	wc.lpszClassName = L"MY_CLASS";
-	wc.hInstance = hInstance;
 	wc.hbrBackground = (HBRUSH)GetStockObject(LTGRAY_BRUSH);
 
-	if (RegisterClass(&wc) == 0) {
-		MessageBox(0, L"Could not register class.", 0, 0);
-	}
+	RegisterClass(&wc);
+	CreateWindow(L"MY_CLASS", L"My Window", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 500, 500, 0, 0, hInstance, 0);
 
-	CreateWindow(L"MY_CLASS", L"My Window", WS_OVERLAPPEDWINDOW|WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 600, 600, 0, 0, hInstance, 0);
-	
 	MSG msg;
 	while (GetMessage(&msg, 0, 0, 0)) {
 		TranslateMessage(&msg);
