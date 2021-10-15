e12f291d3000c4954570fec4414e9dc7fa45d4c5
torhen <torhen@gmail.com>
2021-10-15 21:18:03 +0200

    Add files via upload

--- 05_Backbuffer.cpp
+++ 05_Backbuffer.cpp
@@ -1,41 +1,44 @@
 #include <windows.h>
 
-BITMAPINFO g_bmi;
-UINT32 *pixel_array;
+const int gBmpWidth = 100;
+const int gBmpHeight = 100;
+BITMAPINFO gBmi;
+UINT32 gPixels[gBmpWidth * gBmpHeight];
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam) {
-	PAINTSTRUCT ps = {};
+
+	PAINTSTRUCT ps;
 	HDC hDC;
 	RECT cr;
-	// Bitmap dimensions
-	int w = 10, h = 10;
 
 	switch (Msg) {
-	case WM_CREATE:
-		g_bmi.bmiHeader = {};
-		g_bmi.bmiHeader.biSize = sizeof(g_bmi.bmiHeader);
-		g_bmi.bmiHeader.biWidth = w;
-		g_bmi.bmiHeader.biHeight = h;
-		g_bmi.bmiHeader.biBitCount = 32;
-		g_bmi.bmiHeader.biPlanes = 1;
 
-		pixel_array = new UINT32[w * h];
-		// set first pixel to blue
-		pixel_array[0] = 0x000000FF;
-		
+	case WM_CREATE:
+		gBmi = {};
+		gBmi.bmiHeader.biSize = sizeof(gBmi.bmiHeader);
+		gBmi.bmiHeader.biPlanes = 1;
+		gBmi.bmiHeader.biBitCount = 32;
+		gBmi.bmiHeader.biWidth = gBmpWidth;
+		gBmi.bmiHeader.biHeight = gBmpHeight;
+
+		return 0;
+
 	case WM_PAINT:
 		hDC = BeginPaint(hWnd, &ps);
 
+		for (int i = 0; i < gBmpWidth * gBmpHeight; i++) {
+			gPixels[i] = RGB(rand(), rand(), rand());
+		}
+
 		GetClientRect(hWnd, &cr);
-		StretchDIBits(hDC, cr.left, cr.top, cr.right, cr.bottom, 0, 0, w, h, pixel_array, &g_bmi, 0, SRCCOPY);
-
+		if (StretchDIBits(hDC, 0, 0, cr.right, cr.bottom, 0, 0, gBmpWidth, gBmpHeight, gPixels, &gBmi, 0, SRCCOPY) == 0) {
+			MessageBox(0, L"StretchDIBs failed", 0, 0);
+			return 0;
+		}
 		EndPaint(hWnd, &ps);
-
-		return 0; 
-
+		return 0;
 	case WM_DESTROY:
 		PostQuitMessage(0);
-		return 0;
 	}
 
 	return DefWindowProc(hWnd, Msg, wParam, lParam);
@@ -44,19 +47,26 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam) {
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 	WNDCLASS wc = {};
+	HWND hWnd;
+
 	wc.hInstance = hInstance;
 	wc.lpfnWndProc = WndProc;
-	wc.lpszClassName = L"MY_CLASS";
-	wc.hbrBackground = CreateSolidBrush(RGB(255, 0, 255));
-	wc.style = CS_HREDRAW | CS_VREDRAW;
-	RegisterClass(&wc);
+	wc.lpszClassName = L"MY_WINDOW_CLASS";
+	wc.style = CS_VREDRAW | CS_HREDRAW;
 
-	CreateWindow(L"MY_CLASS", L"My Window", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 500, 500, 0, 0, hInstance, 0);
+	if (!RegisterClass(&wc)) {
+		MessageBox(0, L"Could not register class", 0, 0);
+		return 0;
+	}
+
+	hWnd = CreateWindow(L"MY_WINDOW_CLASS", L"My Window", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 
+		CW_USEDEFAULT, CW_USEDEFAULT, gBmpWidth * 5, gBmpHeight * 5 , 0, 0, hInstance, 0);
 
 	MSG msg;
 	while (GetMessage(&msg, 0, 0, 0)) {
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
+
 	return 0;
 }
