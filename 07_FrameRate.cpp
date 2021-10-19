0706e87bfb06316203a672efaab79af8f968f3e3
torhen <torhen@gmail.com>
2021-10-19 11:53:39 +0200

    Add files via upload

--- 07_FrameRate.cpp
+++ 07_FrameRate.cpp
@@ -3,8 +3,25 @@
 
 const int BMPW = 20;
 const int BMPH = 20;
+int gFrameRate = 10;
 BITMAPINFO gBmi;
 UINT32 gPixel[BMPW * BMPH];
+long int gFrameCount = 0;
+
+int update() {
+
+	// all pixels black
+	for (int i = 0; i < BMPW * BMPH; i++) {
+		gPixel[i] = 0;
+	}
+
+	// on pixel red
+	int n = gFrameCount % (BMPW * BMPH);
+	gPixel[n] = RGB(0, 0, 255);
+
+
+	return 0;
+}
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam) {
 
@@ -36,7 +53,6 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int iCmd
 	MSG msg;
 	RECT cr;
 	HDC hDC;
-	int test = 0;
 	LARGE_INTEGER start, curr, second;
 
 	wc.hInstance = hInstance;
@@ -49,7 +65,6 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int iCmd
 	}
 
 	hWnd = CreateWindow(L"MY_CLASS", L"My Window", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 500, 500, 0, 0, hInstance, 0);
-
 
 	while (1) {
 
@@ -67,32 +82,19 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int iCmd
 		hDC = GetDC(hWnd);
 		GetClientRect(hWnd, &cr);
 
-		// all pixels black
-		for (int i = 0; i < BMPW * BMPH; i++) {
-			gPixel[i] = 0;
-		}
-
-		// on pixel red
-		gPixel[test] = RGB(0, 0, 255);
-		if (test >= BMPW * BMPH) {
-			test = 0;
-		}
-		else {
-			test++;
-		}
-
+		gFrameCount++;
+		update();
 
 		StretchDIBits(hDC, 0, 0, cr.right, cr.bottom, 0, 0, BMPW, BMPH, gPixel, &gBmi, 0, SRCCOPY);
 		ReleaseDC(hWnd, hDC);
 
 		LARGE_INTEGER second;
 		QueryPerformanceFrequency(&second);
-
 		// wait till frame end
 		while (1) {
 			QueryPerformanceCounter(&curr);
 			Sleep(0);
-			if (curr.QuadPart > start.QuadPart + second.QuadPart / 60) {
+			if (curr.QuadPart > start.QuadPart + second.QuadPart / gFrameRate) {
 				break;
 			}
 		}
