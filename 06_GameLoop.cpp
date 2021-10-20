9e32161d0ddce7ca6e214e4cb5865b8522851e61
torhen <torhen@gmail.com>
2021-10-20 23:47:42 +0200

    Add files via upload

--- 06_GameLoop.cpp
+++ 06_GameLoop.cpp
@@ -1,28 +1,28 @@
 #include <windows.h>
 
-const int BMP_WIDTH = 160;
-const int BMP_HEIGHT = 90;
-BITMAPINFO gBmi;
-UINT32 gPixels[BMP_WIDTH * BMP_HEIGHT];
+const int BMPW = 10;
+const int BMPH = 10;
+long int gFrameCount;
+UINT32 gPixel[BMPW * BMPH];
+
+void paint(HDC hDC, int x, int y) {
+	BITMAPINFO Bmi = {};
+	Bmi.bmiHeader.biSize = sizeof(Bmi.bmiHeader);
+	Bmi.bmiHeader.biWidth = BMPW;
+	Bmi.bmiHeader.biHeight = -BMPH;
+	Bmi.bmiHeader.biPlanes = 1;
+	Bmi.bmiHeader.biBitCount = 32;
+	gPixel[0] = 0xFFFF00FF * (gFrameCount % 2);
+	StretchDIBits(hDC, 0, 0, x, y, 0, 0, BMPW, BMPH, gPixel, &Bmi, 0, SRCCOPY);
+}
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam) {
-
-	PAINTSTRUCT ps;
-	HDC hDC;
-
 	switch (Msg) {
-	case WM_CREATE:
-		gBmi = {};
-		gBmi.bmiHeader.biSize = sizeof(gBmi.bmiHeader);
-		gBmi.bmiHeader.biPlanes = 1;
-		gBmi.bmiHeader.biBitCount = 32;
-		gBmi.bmiHeader.biWidth = BMP_WIDTH;
-		gBmi.bmiHeader.biHeight = BMP_HEIGHT;
 
-		return 0;
-
-	case WM_DESTROY:
+	case WM_CLOSE:
+	{
 		PostQuitMessage(0);
+	}return 0;
 	}
 
 	return DefWindowProc(hWnd, Msg, wParam, lParam);
@@ -31,24 +31,20 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam) {
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 	WNDCLASS wc = {};
-	HWND hWnd;
-	RECT cr;
-	static int iCount = 0;
-
-	wc.hInstance = hInstance;
 	wc.lpfnWndProc = WndProc;
-	wc.lpszClassName = L"MY_WINDOW_CLASS";
-	wc.style = CS_VREDRAW | CS_HREDRAW;
+	wc.hInstance = hInstance;
+	wc.lpszClassName = L"MY_CLASS";
+	wc.style = CS_HREDRAW | CS_VREDRAW;
 
 	if (!RegisterClass(&wc)) {
-		MessageBox(0, L"Could not register class", 0, 0);
+		MessageBox(0, L"Can't register Window Class", 0, 0);
 		return 0;
 	}
 
-	hWnd = CreateWindow(L"MY_WINDOW_CLASS", L"My Window", WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-		CW_USEDEFAULT, CW_USEDEFAULT, BMP_WIDTH * 8, BMP_HEIGHT * 8, 0, 0, hInstance, 0);
+	HWND hWnd = CreateWindow(wc.lpszClassName, L"My Window", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 0, 0, 500, 500, 0, 0, hInstance, 0);
 
 	MSG msg;
+	HDC hDC;
 	while (1) {
 		while (PeekMessage(&msg, 0, 0, 0, PM_REMOVE)) {
 			if (msg.message == WM_QUIT) {
@@ -58,20 +54,13 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPSTR, _In
 			DispatchMessage(&msg);
 		}
 
-		// Set pixel
-		for (int i = 0; i < BMP_WIDTH * BMP_HEIGHT; i++) {
-			gPixels[i] = RGB(rand(), rand(), rand());
-		}
-
-		//Draw bitmap
+		gFrameCount++;
+		hDC = GetDC(hWnd);
+		RECT cr;
 		GetClientRect(hWnd, &cr);
-		HDC hDC = GetDC(hWnd);
-		StretchDIBits(hDC, 0, 0, cr.right, cr.bottom, 0, 0, BMP_WIDTH, BMP_HEIGHT, &gPixels, &gBmi, 0, SRCCOPY);
-		TCHAR buffer[100];
-		iCount++;
-		wsprintf(buffer, L"%d k frames", iCount/1000);
-		DrawText(hDC, buffer, -1, &cr, 0);
+		paint(hDC, cr.right, cr.bottom);
 		ReleaseDC(hWnd, hDC);
+
 	}
 
 	return 0;
