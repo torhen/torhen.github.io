4083676c22711372a413b2c6d5016308953f4153
torhen <torhen@gmail.com>
2021-10-20 23:34:00 +0200

    Add files via upload

--- 01_MessageBox.cpp
+++ 01_MessageBox.cpp
@@ -1,7 +1,6 @@
 #include <windows.h>
 
-int _stdcall WinMain(HINSTANCE hInstance, HINSTANCE, char* , int){
-
-	MessageBox(0, L"Hello World!", L"Greeting", 0);
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int) {
+	MessageBox(0, L"Hello World!", 0, 0);
 	return 0;
 }
