264b44e33a481f8ecbb0ae6e7adffc9caa491e99
torhen <torhen@gmail.com>
2021-10-21 00:08:25 +0200

    Add files via upload

--- 07_FrameRate.cpp
+++ 07_FrameRate.cpp
@@ -1,74 +1,59 @@
 #include <windows.h>
-#include <stdio.h>
 
-// define dimension and framerate
-const int BMPW = 20;
-const int BMPH = 20;
+const int BMPW = 10;
+const int BMPH = 10;
 int gFrameRate = 2;
 
-BITMAPINFO gBmi;
+long int gFrameCount;
 UINT32 gPixel[BMPW * BMPH];
-long int gFrameCount = 0;
 
-int update(){
-	gPixel[0 + 0 * BMPW] = 0xFFFF0000 * (gFrameCount % 2);
-	return 0;
+
+void paint(HDC hDC, int x, int y) {
+	BITMAPINFO Bmi = {};
+	Bmi.bmiHeader.biSize = sizeof(Bmi.bmiHeader);
+	Bmi.bmiHeader.biWidth = BMPW;
+	Bmi.bmiHeader.biHeight = -BMPH;
+	Bmi.bmiHeader.biPlanes = 1;
+	Bmi.bmiHeader.biBitCount = 32;
+	gPixel[0] = 0xFFFF00FF * (gFrameCount % 2);
+	StretchDIBits(hDC, 0, 0, x, y, 0, 0, BMPW, BMPH, gPixel, &Bmi, 0, SRCCOPY);
 }
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam) {
-
 	switch (Msg) {
-	case WM_CREATE:
-		// initialize BITMAPINFO
-		gBmi.bmiHeader.biSize = sizeof(gBmi.bmiHeader);
-		gBmi.bmiHeader.biWidth = BMPW;
-		gBmi.bmiHeader.biHeight = -BMPH; // minus makes ero UPPER left
-		gBmi.bmiHeader.biPlanes = 1;
-		gBmi.bmiHeader.biBitCount = 32;
-		return 0;
 
 	case WM_CLOSE:
+	{
 		PostQuitMessage(0);
-		return 0;
+	}return 0;
 	}
 
 	return DefWindowProc(hWnd, Msg, wParam, lParam);
 }
 
-int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int iCmd) {
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
-	HINSTANCE hInstance = GetModuleHandle(0);
 	WNDCLASS wc = {};
-	HWND hWnd;
-	MSG msg;
-	RECT cr;
-	HDC hDC;
-	LARGE_INTEGER liSecond, liStart, liNow;
-
-	// Register window class
-	wc.hInstance = hInstance;
 	wc.lpfnWndProc = WndProc;
+	wc.hInstance = hInstance;
 	wc.lpszClassName = L"MY_CLASS";
+	wc.style = CS_HREDRAW | CS_VREDRAW;
 
-	if (RegisterClass(&wc) == 0) {
-		MessageBox(0, L"Could not register class", 0, 0);
+	if (!RegisterClass(&wc)) {
+		MessageBox(0, L"Can't register Window Class", 0, 0);
 		return 0;
 	}
 
-	// Create Window
-	hWnd = CreateWindow(L"MY_CLASS", L"Constant Frame Rate", WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-		CW_USEDEFAULT, CW_USEDEFAULT, 500, 500, 0, 0, hInstance, 0);
+	HWND hWnd = CreateWindow(wc.lpszClassName, L"My Window", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 0, 0, 500, 500, 0, 0, hInstance, 0);
 
-	// Get frequency of performance counter
+	MSG msg;
+	HDC hDC;
+	LARGE_INTEGER liSecond, liStart, liNow;
 	QueryPerformanceFrequency(&liSecond);
-	
-	// Game Loop
-	for(;;){
 
-		// Get start value of performance counter
+	while (1) {
 		QueryPerformanceCounter(&liStart);
 
-		// process all messages in queue
 		while (PeekMessage(&msg, 0, 0, 0, PM_REMOVE)) {
 			if (msg.message == WM_QUIT) {
 				return 0;
@@ -77,24 +62,23 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int iCmd
 			DispatchMessage(&msg);
 		}
 
-		// update buffer
 		gFrameCount++;
-		update();
-
-		// display buffer
 		hDC = GetDC(hWnd);
+		RECT cr;
 		GetClientRect(hWnd, &cr);
-		StretchDIBits(hDC, 0, 0, cr.right, cr.bottom, 0, 0, BMPW, BMPH, gPixel, &gBmi, 0, SRCCOPY);
+		paint(hDC, cr.right, cr.bottom);
 		ReleaseDC(hWnd, hDC);
 
-		// loop until the target time of frame is reached
+
 		while (1) {
 			QueryPerformanceCounter(&liNow);
-			Sleep(0);
-			if (liNow.QuadPart > liStart.QuadPart + liSecond.QuadPart / gFrameRate) {
+			if (liNow.QuadPart >= liStart.QuadPart + liSecond.QuadPart / gFrameRate) {
 				break;
 			}
+			Sleep(0);
 		}
+
 	}
+
 	return 0;
 }
