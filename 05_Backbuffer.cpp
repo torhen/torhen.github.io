17eb17335ae62499fd0bed1870b66db97d456e3d
torhen <torhen@gmail.com>
2021-10-22 00:03:47 +0200

    Add files via upload

--- 05_Backbuffer.cpp
+++ 05_Backbuffer.cpp
@@ -4,7 +4,7 @@ const int BMPW = 10;
 const int BMPH = 10;
 UINT32 gPixel[BMPW * BMPH];
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam) {
+LRESULT CALLBACK wnd_proc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam) {
 	switch (Msg) {
 	case WM_PAINT:
 	{
@@ -37,7 +37,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam) {
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 	WNDCLASS wc = {};
-	wc.lpfnWndProc = WndProc;
+	wc.lpfnWndProc = wnd_proc;
 	wc.hInstance = hInstance;
 	wc.lpszClassName = L"MY_CLASS";
 	wc.style = CS_HREDRAW | CS_VREDRAW;
