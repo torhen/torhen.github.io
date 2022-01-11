dd1a3a1e0a0d99482540b2302104664f6869639c
torhen <torhen@gmail.com>
2022-01-12 00:12:40 +0100

    Add files via upload

---
+++ stubs.py
@@ -0,0 +1,13 @@
+# import into transcrypt file to avoid editor errors
+# from stubs import *  #__: skip
+
+def alert(s): pass
+
+class Console:
+    def log(*args): pass
+console = Console()
+
+class Dokument():
+    def getElementbyId(id): pass
+document = Dokument()
+
