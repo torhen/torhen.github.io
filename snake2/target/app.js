c96eddcb262a7c0e168ffd6561b23cea01ad4b89
torhen <torhen@gmail.com>
2022-01-14 22:41:26 +0100

    Add files via upload

--- snake2/target/app.js
+++ snake2/target/app.js
@@ -1,4 +1,4 @@
-// Transcrypt'ed from Python, 2022-01-14 10:14:39
+// Transcrypt'ed from Python, 2022-01-14 22:40:13
 var random = {};
 var time = {};
 import {AssertionError, AttributeError, BaseException, DeprecationWarning, Exception, IndexError, IterableError, KeyError, NotImplementedError, RuntimeWarning, StopIteration, UserWarning, ValueError, Warning, __JsIterator__, __PyIterator__, __Terminal__, __add__, __and__, __call__, __class__, __envir__, __eq__, __floordiv__, __ge__, __get__, __getcm__, __getitem__, __getslice__, __getsm__, __gt__, __i__, __iadd__, __iand__, __idiv__, __ijsmod__, __ilshift__, __imatmul__, __imod__, __imul__, __in__, __init__, __ior__, __ipow__, __irshift__, __isub__, __ixor__, __jsUsePyNext__, __jsmod__, __k__, __kwargtrans__, __le__, __lshift__, __lt__, __matmul__, __mergefields__, __mergekwargtrans__, __mod__, __mul__, __ne__, __neg__, __nest__, __or__, __pow__, __pragma__, __pyUseJsNext__, __rshift__, __setitem__, __setproperty__, __setslice__, __sort__, __specialattrib__, __sub__, __super__, __t__, __terminal__, __truediv__, __withblock__, __xor__, abs, all, any, assert, bool, bytearray, bytes, callable, chr, copy, deepcopy, delattr, dict, dir, divmod, enumerate, filter, float, getattr, hasattr, input, int, isinstance, issubclass, len, list, map, max, min, object, ord, pow, print, property, py_TypeError, py_iter, py_metatype, py_next, py_reversed, py_typeof, range, repr, round, set, setattr, sorted, str, sum, tuple, zip} from './org.transcrypt.__runtime__.js';
@@ -208,6 +208,18 @@ export var Game =  __class__ ('Game', [object], {
 		}
 	});}
 });
+export var myButton = document.getElementById ('button_up');
+myButton.style.width = '600px';
+myButton.style.height = '100px';
+var myButton = document.getElementById ('button_left');
+myButton.style.width = '300px';
+myButton.style.height = '100px';
+var myButton = document.getElementById ('button_right');
+myButton.style.width = '300px';
+myButton.style.height = '100px';
+var myButton = document.getElementById ('button_down');
+myButton.style.width = '600px';
+myButton.style.height = '100px';
 export var game = Game ();
 export var g_time = 0;
 export var g_takt = 0.2;
--- snake2/target/org.transcrypt.__runtime__.js
+++ snake2/target/org.transcrypt.__runtime__.js
@@ -1,4 +1,4 @@
-// Transcrypt'ed from Python, 2022-01-14 10:14:39
+// Transcrypt'ed from Python, 2022-01-14 22:40:13
 var __name__ = 'org.transcrypt.__runtime__';
 export var __envir__ = {};
 __envir__.interpreter_name = 'python';
