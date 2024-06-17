7de492d3c2958ac4f4076263198830958fc3df72
torhen <torhen@gmail.com>
2024-06-17 11:02:16 +0200

    Add files via upload

--- pyodide_matplotlib/script.py
+++ pyodide_matplotlib/script.py
@@ -1,19 +1,34 @@
+from pyodide.ffi import create_proxy
 from matplotlib import pyplot as plt
 import random
 import io
 import base64
 import js
 
+
+status1 = js.document.getElementById("status1")
+status1.innerHTML = "ready."
 iMyPlot = js.document.getElementById("img1")
+button1 = js.document.getElementById("button1")
 
-fig, ax = plt.subplots()
-ax.plot([random.random() for _ in range(100)])
 
-img =  io.StringIO()
-plt.savefig(img, format="svg")
-img.seek(0)
-imgdata = img.read()
+def make_plot(para):
+  
 
-iMyPlot.setAttribute("src","data:image/svg+xml;base64," + str(base64.b64encode(imgdata.encode()), "ascii"))
+    fig, ax = plt.subplots()
+    ax.plot([random.random() for _ in range(100)])
 
-print('finished.')
+    img =  io.StringIO()
+    plt.savefig(img, format="svg")
+    img.seek(0)
+    imgdata = img.read()
+
+    iMyPlot.setAttribute("src","data:image/svg+xml;base64," + str(base64.b64encode(imgdata.encode()), "ascii"))
+
+    print('finished.')
+
+
+proxy_make_plot = create_proxy(make_plot)
+
+button1.addEventListener('click', proxy_make_plot)
+make_plot(0)
