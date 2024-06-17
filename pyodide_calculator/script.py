d6ff475dd9a05718cae34cb9e31bba8965645bc4
torhen <torhen@gmail.com>
2024-06-17 09:52:32 +0200

    Add files via upload

--- pyodide_calculator/script.py
+++ pyodide_calculator/script.py
@@ -1,10 +1,28 @@
-import js
+from js import document
+from pyodide.ffi import create_proxy
+import datetime
 from math import *
 
-s = js.document.getElementById('input').value
-try:
-    r = eval(s)
-except Exception as error:
-    r = 'ERROR:' + str(error)
 
-js.document.getElementById("output").innerHTML = r
+status1 = document.getElementById('status1')
+status1.innerHTML = 'ready.'
+input1 = document.getElementById('input1')
+output1 = document.getElementById('output1')
+
+def calc(*args):
+    s = input1.value
+    try:
+        r = eval(s)
+    except Exception as e:
+        r = str(e)
+    output1.value = str(r)
+
+proxy_calc = create_proxy(calc)
+
+input1.addEventListener('change', proxy_calc)
+calc()
+
+# Store proxy_f in Python then later:
+# document.body.removeEventListener('click', proxy_f)
+# proxy_f.destroy()
+
